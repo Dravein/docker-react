90aa7def21aacd8c8c2cdd9b69327234eb354237
Tamás Toldi <damonic11@gmail.com>
2024-11-21 11:36:01 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Csumika! Learn React!
+          Csumika Feature Branch! Learn React!
         </a>
       </header>
     </div>
